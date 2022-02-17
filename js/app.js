7c015715445d1ec8f26ad5f9e59801e04cbd95ca
Saklain Mustak <saklainmustak@sakmac.local>
2022-02-17 09:23:07 +0600

    console removed

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 // get valur from input
 function getValau(){
+    //income
     const incomeField = document.getElementById('income-field');
     const income = parseFloat(incomeField.value);
     console.log(income);
     const foodField = document.getElementById('food-field');
     const food = parseFloat(foodField.value);
-    console.log(food);
     const rentField = document.getElementById('rent-field');
     const rent = parseFloat(rentField.value);
-    console.log(rent);
     const clothesField = document.getElementById('clothes-field');
     const clothes = parseFloat(clothesField.value);
-    console.log(clothes);
+    const totalExpense = document.getElementById('total-expense');
+    totalExpense.innerText  = food + rent + clothes;
+    const balance = document.getElementById('Balance');
+     balance.innerText = income - totalExpense.innerText;
     
 }
 
