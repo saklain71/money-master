ac17980996bf0da6ed045eb1934a455120de7a62
Saklain Mustak <saklainmustak@sakmac.local>
2022-02-17 09:58:39 +0600

    calculation balance validate

--- js/app.js
+++ js/app.js
@@ -1,20 +1,36 @@
+function inputAmount(inputId){
+    const input = document.getElementById(inputId);
+    const amount = parseFloat(input.value);
+    return amount;
+}
+
 // get valur from input
 function getValau(){
     //income
-    const incomeField = document.getElementById('income-field');
-    const income = parseFloat(incomeField.value);
-    console.log(income);
-    const foodField = document.getElementById('food-field');
-    const food = parseFloat(foodField.value);
-    const rentField = document.getElementById('rent-field');
-    const rent = parseFloat(rentField.value);
-    const clothesField = document.getElementById('clothes-field');
-    const clothes = parseFloat(clothesField.value);
+    const income = inputAmount('income-field');
+    // const incomeField = document.getElementById('income-field');
+    // const income = parseFloat(incomeField.value);
+    // const foodField = document.getElementById('food-field');
+    // const food = parseFloat(foodField.value);
+    // Expenses
+    const food = inputAmount('food-field');
+    // const rentField = document.getElementById('rent-field');
+    // const rent = parseFloat(rentField.value);
+    const rent = inputAmount('rent-field');
+    const clothes = inputAmount('clothes-field');
+    // const clothesField = document.getElementById('clothes-field');
+    // const clothes = parseFloat(clothesField.value);
+
     const totalExpense = document.getElementById('total-expense');
     totalExpense.innerText  = food + rent + clothes;
     const balance = document.getElementById('Balance');
-     balance.innerText = income - totalExpense.innerText;
-    
+    balance.innerText = income - totalExpense.innerText;
+    if(income > totalExpense.innerText){
+        balance.innerText = income - totalExpense.innerText;
+    }
+    else{
+        alert('Invalid Income Amount');
+    }
 }
 
 // calculation button
